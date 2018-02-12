31564f6d752e848a2946323f3229f61076c8f270
Josh Justice <jjustice@bignerdranch.com>
2018-02-12 09:54:19 -0500

    Specify production environment

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,5 +6,8 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   plugins: [
     new CleanWebpackPlugin(['dist']),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
   ],
 });
