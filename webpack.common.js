31ac19a7a634770c0b1fae4ceed7994815188fa5
Josh Justice <jjustice@bignerdranch.com>
2018-02-12 10:20:07 -0500

    Do not transpile es6 modules, allowing tree shaking

--- webpack.common.js
+++ webpack.common.js
@@ -21,7 +21,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [
-              'env',
+              ['env', {'modules': false}],
               'react',
             ],
           },
