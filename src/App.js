b187d0a70f04d698826fea3a674826930b2598f1
Josh Justice <jjustice@bignerdranch.com>
2018-02-12 13:19:13 -0500

    Add restaurant app screens

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import logo from './logo.png';
+import React, { Component } from 'react';
+import restaurants from './restaurants.json';
 
-const App = () =>
-  <div>
-    <img src={logo} alt="React Logo" />
-    Hello, React!
-  </div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      restaurants: restaurants,
+    };
+  }
+
+  render() {
+    const { restaurants } = this.state;
+    return (
+      <div>
+        <h1>Restaurants</h1>
+        <ul>
+          { restaurants.map(r => <li>{ r.name }</li>) }
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
