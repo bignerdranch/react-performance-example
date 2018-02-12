ad443ebbe75a159eed82e36d93c0c8fe90e19e69
Josh Justice <jjustice@bignerdranch.com>
2018-02-12 11:05:19 -0500

    Clean production build folder

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,10 @@
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+  ],
 });
