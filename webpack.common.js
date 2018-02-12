8a8091e8eaa4e3e6aceaa5066df9b5023dbe3dc9
Josh Justice <jjustice@bignerdranch.com>
2018-02-12 14:04:46 -0500

    Prepare config for future diffs

--- webpack.common.js
+++ webpack.common.js
@@ -20,7 +20,10 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env', 'react'],
+            presets: [
+              'env',
+              'react',
+            ],
           },
         },
       },
