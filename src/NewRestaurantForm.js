b659070246bf0d72b84f130fb5bdbc6082437562
Josh Justice <jjustice@bignerdranch.com>
2018-02-14 11:43:03 -0500

    Make text field a controlled component

--- src/NewRestaurantForm.js
+++ src/NewRestaurantForm.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 
 export default class NewRestaurantForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newRestaurantName: '',
+    };
+  }
+
+  updateRestaurantName(event) {
+    this.setState({ newRestaurantName: event.target.value });
+  }
+
   render() {
+    const { newRestaurantName } = this.state;
     return (
       <div>
         <input
           type="text"
           data-test="newRestaurantName"
+          value={newRestaurantName}
+          onChange={e => this.updateRestaurantName(e)}
         />
         <button
           data-test="saveButton"
