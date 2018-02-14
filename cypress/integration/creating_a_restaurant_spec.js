8b4ab5060eca4ae4b58137026396ff6d15778aef
Josh Justice <jjustice@bignerdranch.com>
2018-02-14 10:20:28 -0500

    Simplify test

--- cypress/integration/creating_a_restaurant_spec.js
+++ cypress/integration/creating_a_restaurant_spec.js
@@ -2,9 +2,6 @@ describe('Creating a restaurant', () => {
   it('adds the restaurant to the list', () => {
     cy.visit('http://localhost:8080/');
 
-    cy.get("[data-test='addRestaurantButton']")
-      .click();
-
     cy.get("[data-test='newRestaurantName']")
       .type('Steak Place');
 
